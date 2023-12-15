d8f7a93b68aac54876376edfeddf2dacc62d24c7
msolrod585 <148794116+msolrod585@users.noreply.github.com>
2023-12-15 18:06:55 +0100

    Add files via upload

---
+++ ejerciciojava.java
@@ -0,0 +1,15 @@
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Este programa realiza el producto de los 10 primeros números impares ");
+    int i;
+    int multiplo = 1;
+    for (i=1; i<20; i += 2) {	
+        multiplo = multiplo * i;
+    sc.close();
+    }
+    System.out.println("Este programa realiza el producto de los 10 primeros números impares, el total es: " + multiplo);
+
+
+}
+
